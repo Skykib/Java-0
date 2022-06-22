45cf23bfec6ef651f73bb2d85b800bedeae362d9
Skykib <skykib@hotmail.com>
2022-06-21 20:37:24 -0500

    suma

---
+++ suma.java
@@ -0,0 +1,13 @@
+public class suma{
+  public static void main (String args[]){
+    
+    int numeroUno = 5;
+    int numeroDos = 2;
+    int resultado = 0;
+    
+    resultado = numeroUno + numeroDos;
+
+    System.out.println("El Resultado de la suma es " + resultado);
+
+ }
+}
